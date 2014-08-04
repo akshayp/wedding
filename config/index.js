9ea7937727fb8b7114a55f9208e3cde69e8405ed
Akshay Patel <akshayrpatel@gmail.com>
2014-08-04 03:44:55 -0700

    Hook up config

---
+++ config/index.js
@@ -0,0 +1,39 @@
+var env = process.env,
+    path = require('path');
+
+module.exports = {
+
+    database: env.DATABASE_URL,
+
+    session: {
+        keys   : ['akshali.session'],
+        secret: env.SESSION_SECRET,
+
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 1 week
+        }
+    },
+
+    invitationSecret: env.INVITATION_SECRET,
+
+    mailgun: {
+        endpoint: env.MAILGUN_API_SERVER && (env.MAILGUN_API_SERVER + '/'),
+        domain : env.MAILGUN_DOMAIN,
+        secret : env.MAILGUN_API_KEY
+    },
+
+    email: {
+        from: 'Vaishali & Akshay <rsvp@akshali.me>'
+    },
+
+    dirs: {
+        //views   : path.resolve('views/pages/'),
+        //layouts : path.resolve('views/layouts/'),
+        //partials: path.resolve('views/partials/'),
+        emails  : path.resolve('views/emails/')
+    },
+
+    date: new Date('Sat Nov 1 2014 6:30:00 GMT-0400 (EDT)'),
+
+    version: require('../package').version
+};
