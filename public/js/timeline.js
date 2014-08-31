cb5ba5e664c15c5164b0781ec50a055a0367ca7e
Akshay Patel <akshayrpatel@gmail.com>
2014-08-31 09:29:35 -0700

    Fix link

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -9,7 +9,7 @@
     }
 
     timelineblocks.forEach(function (node) {
-        if(!inViewport(node, 400)) {
+        if(!inViewport(node, 350)) {
             var hideEls = arrayify(node.querySelectorAll('.timeline-marker, .timeline-content'));
             hideEls.forEach(function (el) {
                 el.className += ' is-hidden';
