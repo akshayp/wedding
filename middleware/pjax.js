93f6f0cec5ef6c8ef0ba6e5d96e318846985001d
Akshay Patel <akshayrpatel@gmail.com>
2014-09-06 01:30:06 -0700

    Add pjax middleware to render layout without body

---
+++ middleware/pjax.js
@@ -0,0 +1,15 @@
+module.exports = function (req, res, next) {
+    res.render_ = res.render;
+
+    res.render = function (view, options, fn) {
+        options = options || {};
+
+        if (req.header('x-requested-with') == 'XMLHttpRequest') {
+            options.layout = false;
+        }
+
+        res.render_(view, options, fn);
+    }
+
+    next();
+}
