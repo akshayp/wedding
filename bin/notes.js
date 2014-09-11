0569e8c9366a489598e71e2d195ac9041b17b4ef
Akshay Patel <akshayrpatel@gmail.com>
2014-09-11 00:32:35 -0700

    Add script to print notes

---
+++ bin/notes.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+/*jshint unused: false*/
+
+var fs = require('fs'),
+    config = require('../config'),
+    colors = require('colors'),
+    guests  = require('../lib/guests');
+
+function importData() {
+
+    guests.loadGuests(function(err, data) {
+        for (var guest in data) {
+            var oGuest = data[guest];
+            if (oGuest.attending !== '') {
+                console.log(oGuest.name.yellow + " said " + oGuest.notes.blue);
+            }
+        }
+        process.exit();
+    });
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdout.write('Do you want to log data into the console ? (yes/no): ');
+
+process.stdin.once('data', function (answer) {
+    answer = answer.toString().trim().toLowerCase();
+
+    if (answer === 'yes') {
+        importData();
+    } else {
+        console.log('Aborting!'.red);
+        process.exit();
+    }
+});
