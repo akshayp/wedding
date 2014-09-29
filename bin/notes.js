b71581a82e8f32f4259ec2f1e589c115f179975c
Akshay Patel <akshayrpatel@gmail.com>
2014-09-28 23:08:14 -0700

    Simplify Notes script

--- bin/notes.js
+++ bin/notes.js
@@ -6,31 +6,12 @@ var fs = require('fs'),
     colors = require('colors'),
     guests  = require('../lib/guests');
 
-function importData() {
-
-    guests.loadGuests(function(err, data) {
-        for (var guest in data) {
-            var oGuest = data[guest];
-            if (oGuest.attending !== '' && oGuest.notes) {
-                console.log(oGuest.name.yellow + " said " + oGuest.notes.blue);
-            }
+guests.loadGuests(function(err, data) {
+    for (var guest in data) {
+        var oGuest = data[guest];
+        if (oGuest.attending !== '' && oGuest.notes) {
+            console.log(oGuest.name.yellow + " said " + oGuest.notes.blue);
         }
-        process.exit();
-    });
-}
-
-
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdout.write('Do you want to log data into the console ? (yes/no): ');
-
-process.stdin.once('data', function (answer) {
-    answer = answer.toString().trim().toLowerCase();
-
-    if (answer === 'yes') {
-        importData();
-    } else {
-        console.log('Aborting!'.red);
-        process.exit();
     }
+    process.exit();
 });
